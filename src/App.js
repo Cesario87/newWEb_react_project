20774b86dbaf86d195fab25a0466eee95219724f
Cesario87 <ces.albred@gmail.com>
2023-02-21 19:31:59 +0100

    added/login/logout working

--- src/App.js
+++ src/App.js
@@ -2,14 +2,29 @@ import './App.css';
 import Head from './components/Head';
 import Footer from './components/Footer';
 import Main from './components/Main';
+import { userContext } from './context/userContext';
 import { BrowserRouter } from 'react-router-dom';
+import { useState } from 'react';
 
 function App() {
+  const [user, setUser] = useState("noName"); // state para componente funcional
+
+  const login = (name) => setUser(name); // user = name. Ej. name="Guille" --> user="Guille"
+  const logout = () => setUser(""); // user = ""
+
+  const data = {
+    user,
+    login,
+    logout
+  }
+  
   return (
     <div className="App">
       <BrowserRouter>
-        <Head />
-        <Main />
+        <userContext.Provider value={data}>
+          <Head />
+          <Main />
+        </userContext.Provider>
       </BrowserRouter>
       <Footer />
     </div>
