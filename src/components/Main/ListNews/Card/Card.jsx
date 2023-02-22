bf095c4de45bbff07f415cff3c15affcadddb9dc
Cesario87 <ces.albred@gmail.com>
2023-02-22 17:28:13 +0100

    fixed/articles add to the list

--- src/components/Main/ListNews/Card/Card.jsx
+++ src/components/Main/ListNews/Card/Card.jsx
@@ -2,7 +2,16 @@ import React, { Component } from "react";
 
 class Card extends Component {
   render() {
-    return <div>Card</div>;
+    const { article } = this.props;
+    const abstract = article.abstract;
+    const title = article.headline && article.headline.main ? article.headline.main : article.main;
+    return (
+      <div>
+        <h2>{title}</h2>
+        <p>{abstract}</p>
+        {/* <a href={article.web_url}>Read more</a> */}
+      </div>
+    );
   }
 }
 
